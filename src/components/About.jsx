eef786859c81ba62f6c03e51765b01f1fe259355
Nihal K <nihal04x@gmail.com>
2024-06-09 22:50:02 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import '../styles/About.css';
 
 function About() {
 
-    const myPotrait = '../src/assets/nihalPortrait.png'
+    const myPotrait = '/assets/nihalPortrait.png'
     return (
         <>
             <section className='about-sec' id='about'>
