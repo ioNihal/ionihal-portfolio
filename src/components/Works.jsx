9c582cccc29700bd9c8e753252d39fe4044e8479
Nihal K <nihal04x@gmail.com>
2024-11-09 17:26:29 +0000

    sfdsd

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,6 +4,38 @@ import '../styles/Works.css';
 function Works() {
     const [projects, setProjects] = useState([]);
 
+    const myProjects = [
+        {
+            id: '0',
+            workMockup: 'images/gameCom.png',
+            workTitle: 'GameCom',
+            workDesc: 'GameCom is a Website built on ReactJS for a SA-MP gaming community',
+            workLink: 'https://gamecom-react.vercel.app'
+        },
+        {
+            id: '1',
+            workMockup: 'images/lyrics.png',
+            workTitle: 'Project Lyrics',
+            workDesc: 'Project Lyrics has 3 preloaded song which users can play and control',
+            workLink: 'https://lyrics.ionihal.xyz'
+        },
+        {
+            id: '2',
+            workMockup: 'images/pixels.png',
+            workTitle: 'Project Pixels',
+            workDesc: 'Pixels is an art inspired webpage built using Vanilla JS, HTML, CSS',
+            workLink: 'https://ionihal.github.io/Pixels-art'
+        },
+        {
+            id: '3',
+            workMockup: 'images/splitter.png',
+            workTitle: 'Splitter',
+            workDesc: 'Splitter is a Website built on ReactJS for calculating and splitting group expenses',
+            workLink: 'https://budget-splitter.vercel.app'
+        },
+
+    ];
+
     useEffect(() => {
         // Fetch projects from the backend
         const fetchProjects = async () => {
@@ -24,7 +56,7 @@ function Works() {
             <section className='works-sec' id='works'>
                 <h1>MY PROJECTS</h1>
                 <div className='works-con'>
-                    {
+                    if(projects) {
                         projects.map(project => {
                             return (
                                 <div key={project._id} className='works-card'>
@@ -40,6 +72,22 @@ function Works() {
                             )
                         })
                     }
+                    else {
+                        myProjects.map(project => {
+                            return (
+                                <div key={project.id} className='works-card'>
+                                    <img src={project.workMockup} alt='mockup' />
+                                    <div className='card-title'>
+                                        <div className='card-text'>
+                                            <h5>{project.workTitle}</h5>
+                                            <p>{project.workDesc}</p>
+                                        </div>
+                                        <a href={project.workLink} target='_blank'>VIEW</a>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    } 
                 </div>
             </section>
         </>
